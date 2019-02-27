1cf288ad78755a554946a5addceaa9721cffd950
江振飞 <zhenfei_jiang@dnt.com.cn>
2019-02-27 18:23:12 +0800

    相对路径表示模块

--- feipack/feipack.js
+++ feipack/feipack.js
@@ -6,9 +6,10 @@ let ejs = require('ejs')
 let entry = path.resolve(__dirname,'../js/index.js');
 let output = path.resolve(__dirname,'../public/main.js')
 
+let publicDirName = path.dirname(entry)
+console.log(publicDirName)
 // style-loader
 let styleLoaders = function( source ){
-    console.log(JSON.stringify(source))
     return (
         `let style = document.createElement('style');
         style.innerText = ${JSON.stringify(source).replace(/(\\n)+/g,'')}
@@ -26,7 +27,9 @@ const _modules = {}
  */
 function resolveDevDependencies( entry ){
     // 读出文件的所有内容
-    let content = fs.readFileSync(entry,'utf8');
+    let name = path.join(publicDirName,entry)
+    publicDirName = path.dirname(name)
+    let content = fs.readFileSync(name,'utf8');
     // 该文件模块地址为key存到_modules中
     _modules[entry] = {
         devDependencies: []
@@ -36,19 +39,17 @@ function resolveDevDependencies( entry ){
     }else{
         // 从内容中读出所有的依赖模块
         content = content.replace(/require\(['"](.+?)['"]\)/g,function(){
-            let name = path.join(__dirname,'../js',arguments[1])
-            console.log(name,arguments[1])
+            let pubpath = arguments[1];
             // 读出依赖的内容递归找出每个文件的依赖
-            resolveDevDependencies(name)
-            _modules[entry].devDependencies.push(name)
-            return `require('${name}');`
+            resolveDevDependencies(pubpath)
+            _modules[entry].devDependencies.push(pubpath)
+            return `require('${pubpath}');`
         }).replace(/[\r\n]+/g,';') // 去掉所有的空格和换行
     }
     
     _modules[entry].content = content;
 }
-resolveDevDependencies(entry)
-
+resolveDevDependencies('../js/index.js')
 
 // 最后需要拼成的模板
 let template = `
@@ -60,7 +61,7 @@ let template = `
         modules[moduleId].call(module.exports, module, module.exports, require);
         return module.exports;
     }
-    return require( "<%-entry%>");
+    return require( "../js/index.js");
 })
 ({
     <%for(let i in _modules){
--- public/main.js
+++ public/main.js
@@ -7,26 +7,26 @@
         modules[moduleId].call(module.exports, module, module.exports, require);
         return module.exports;
     }
-    return require( "/Users/fei/webpack/my-webpack/js/index.js");
+    return require( "../js/index.js");
 })
 ({
     
-    "/Users/fei/webpack/my-webpack/js/index.js":
+    "../js/index.js":
     (function(module, exports, require){
-        eval(`let name = require('/Users/fei/webpack/my-webpack/js/people.js');;let css = require('/Users/fei/webpack/my-webpack/css/style.css');;console.log(name)`);
+        eval(`let name = require('./people.js');;let css = require('../css/style.css');;console.log(name)`);
     }),
     
-    "/Users/fei/webpack/my-webpack/js/people.js":
+    "./people.js":
     (function(module, exports, require){
-        eval(`let r = require('/Users/fei/webpack/my-webpack/js/two.js');;console.log( r );module.exports = 'fei11'`);
+        eval(`let r = require('./two.js');;console.log( r );module.exports = 'fei11'`);
     }),
     
-    "/Users/fei/webpack/my-webpack/js/two.js":
+    "./two.js":
     (function(module, exports, require){
         eval(`module.exports = '测试'`);
     }),
     
-    "/Users/fei/webpack/my-webpack/css/style.css":
+    "../css/style.css":
     (function(module, exports, require){
         eval(`let style = document.createElement('style');
         style.innerText = "html,body{    width: 100%;    height: 100%;    background: red;}"
